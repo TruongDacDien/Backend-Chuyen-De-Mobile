cc7078048d3628941e9c6305c43ab1386b5afad8
TruongDacDien <22520248@gm.uit.edu.vn>
2025-10-29 02:29:22 +0700

    Test CI/CD

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,31 @@ const js = require("@eslint/js");
 const globals = require("globals");
 
 module.exports = [
-  js.configs.recommended, // Dùng bộ luật mặc định ESLint
-  {
-    files: ["**/*.js"],    // Áp dụng cho mọi file .js
-    ignores: ["node_modules/**", "dist/**"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,   // Cho phép các biến toàn cục của Node.js (process, __dirname, ...)
-      },
+    js.configs.recommended, // Dùng bộ luật mặc định ESLint
+    {
+        files: ["**/*.js"],    // Áp dụng cho mọi file .js
+        ignores: ["node_modules/**", "dist/**"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,   // Cho phép các biến toàn cục của Node.js (process, __dirname, ...)
+            },
+        },
+        rules: {
+            "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+            "no-console": "off",
+            "no-undef": "error",
+        },
+
     },
-    rules: {
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
-      "no-console": "off",
-      "no-undef": "error",
+    {
+        files: ["**/__tests__/**/*.js", "**/*.test.js", "**/*.spec.js"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest, // ✅ thêm dòng này để ESLint hiểu test/expect/mocks của Jest
+            },
+        },
     },
-  },
 ];
